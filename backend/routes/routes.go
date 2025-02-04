c4561e47a49bd8d76565d864dbe12ceef85c312e
adaniel <adiozdaniel@gmail.com>
2025-02-04 13:40:04 +0300

    chores(routes): serve static files - This helps the browser find static files like images and styling files

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -9,6 +9,10 @@ import (
 
 // Register routes
 func RegisterRoutes(mux *http.ServeMux) http.Handler {
+	//Page routes
+	fs := "frontend"
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(fs))))
+	
 	mux.HandleFunc("/api/auth/register", handlers.RegisterHandler)
 	mux.HandleFunc("/api/auth/login", handlers.LoginHandler)
 	mux.HandleFunc("/api/posts", handlers.PostsHandler)
