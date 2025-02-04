7e5c6ba61f8899f1faf010d1eacb3519cea0b2db
adaniel <adiozdaniel@gmail.com>
2025-02-04 14:48:04 +0300

    fix: references to handlers - Associate  to - Associate  route to

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -10,12 +10,13 @@ import (
 // Register routes
 func RegisterRoutes(mux *http.ServeMux) http.Handler {
 	//Page routes
-	fs := "frontend"
-	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir(fs))))
-	
+	fs := "../frontend"
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(fs))))
+
 	mux.HandleFunc("/api/auth/register", handlers.RegisterHandler)
 	mux.HandleFunc("/api/auth/login", handlers.LoginHandler)
 	mux.HandleFunc("/api/posts", handlers.PostsHandler)
+	mux.HandleFunc("/", handlers.HomePageHandler)
 
 	// CORS middleware
 	handler := middlewares.CorsMiddleware(mux)
