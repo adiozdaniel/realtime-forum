3a0ee8c2d1a3de581303350067b218bc582af505
adaniel <adiozdaniel@gmail.com>
2025-02-05 15:10:43 +0300

    refactor(server): seperate server logic - Created server.go and moved all server related logic to it. - Called server in the main.go to initialise the server. - This makes the code more modular - This obeys the singleton design pattern the application has taken; for consistency

--- backend/main.go
+++ backend/main.go
@@ -1,35 +1,18 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"os"
 
-	"forum/forumapp"
-	"forum/handlers"
-	"forum/routes"
+	"forum/server"
 )
 
 func main() {
-	handlers.InitDB() // Initialize SQLite database connection
-	mux := http.NewServeMux()
-
-	// Register Routes
-	h := routes.RegisterRoutes(mux)
-	_, err := forumapp.ForumInit();
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// Start server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4000"
 	}
 
-	log.Printf("Server starting @http://localhost:%s", port)
-	err = http.ListenAndServe(":"+port, h)
-	if err != nil {
-		log.Fatal("Server failed:", err)
-	}
+	s := server.NewServer(port)
+	s.Start()
 }
---
+++ backend/server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"forum/forumapp"
+	"forum/handlers"
+	"forum/routes"
+)
+
+type Server struct {
+	app  *forumapp.ForumApp
+	repo *handlers.Repo
+	routes *routes.Routes
+	port string
+}
+
+func NewServer(port string) *Server {
+	app, _ := forumapp.ForumInit()
+	repo := handlers.NewRepo(app)
+	routes := routes.NewRoutes(app, repo)
+	return &Server{
+		app:  app,
+		repo: repo,
+		routes: routes,
+		port: port,
+	}
+}
+
+func (s *Server) Start() {
+	handlers.InitDB() // Initialize SQLite database connection
+	mux := http.NewServeMux()
+
+	// Register Routes
+	h := s.routes.RegisterRoutes(mux)
+	_, err := forumapp.ForumInit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Server starting @http://localhost:%s", s.port)
+	err = http.ListenAndServe(":"+s.port, h)
+	if err != nil {
+		log.Fatal("Server failed:", err)
+	}
+}
