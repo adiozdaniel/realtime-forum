1d81ab05071ef70b1c1078ae9c7ba84463eb0ebe
adaniel <adiozdaniel@gmail.com>
2025-02-05 16:38:28 +0300

    feat(routes): add a sign up route - Create `signup` handler to serve signup html, javascript and styling - Add a sign-up router and link

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -9,20 +9,20 @@ import (
 )
 
 type Routes struct {
-	app *forumapp.ForumApp
+	app  *forumapp.ForumApp
 	repo *handlers.Repo
 }
 
 func NewRoutes(app *forumapp.ForumApp, repo *handlers.Repo) *Routes {
 	return &Routes{
-		app: app,
+		app:  app,
 		repo: repo,
 	}
 }
 
 // Register routes
-func(r *Routes) RegisterRoutes(mux *http.ServeMux) http.Handler {
-	//Page routes
+func (r *Routes) RegisterRoutes(mux *http.ServeMux) http.Handler {
+	// Page routes
 	fs := r.app.Tmpls.GetProjectRoute("/static")
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(fs))))
 
@@ -31,6 +31,7 @@ func(r *Routes) RegisterRoutes(mux *http.ServeMux) http.Handler {
 	mux.HandleFunc("/api/posts", handlers.PostsHandler)
 	mux.HandleFunc("/", r.repo.HomePageHandler)
 	mux.HandleFunc("/auth", r.repo.LoginPage)
+	mux.HandleFunc("/auth-sign-up", r.repo.SignUpPage)
 
 	// CORS middleware
 	handler := middlewares.CorsMiddleware(mux)
