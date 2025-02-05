e5832fd840eafb3470eeea478f31807a55481c5c
adaniel <adiozdaniel@gmail.com>
2025-02-05 12:30:41 +0300

    chores(forumapp): add struct initializers - Add  to initialise - Add  to initialise - This approach is suitable for scalability and maintainability

--- backend/forumapp/models.go
+++ backend/forumapp/models.go
@@ -5,10 +5,21 @@ import (
 )
 
 type ForumApp struct {
-	Tmpls *TemplateCache
-	Errors *error
+	Tmpls  *TemplateCache
+	Errors error
 }
 
 type TemplateCache struct {
 	Pages map[string]*template.Template
+}
+
+func newTemplateCache() *TemplateCache {
+	return &TemplateCache{make(map[string]*template.Template)}
+}
+
+func newForumApp() *ForumApp{
+	return &ForumApp{
+		Tmpls: newTemplateCache(),
+		Errors: nil,
+	}
 }
