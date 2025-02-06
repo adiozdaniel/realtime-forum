a4319d2eef368fa6f81a770e207cfb966eb0a4d5
nungo <nued65@gmail.com>
2025-02-06 14:57:51 +0300

    test:add LoginHandler test on http Method

---
+++ backend/handlers/handlers_test.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRegisterHandler(t *testing.T) {
+}
+
+func TestLoginHandler(t *testing.T) {
+	r := httptest.NewRequest(http.MethodGet, "/api/auth/login", nil)
+	w := httptest.NewRecorder()
+	LoginHandler(w, r)
+	if w.Code != http.StatusMethodNotAllowed {
+		t.Errorf("expected %d got %d", http.StatusMethodNotAllowed, w.Code)
+	}
+}
+
+func TestPostsHandler(t *testing.T) {
+}
