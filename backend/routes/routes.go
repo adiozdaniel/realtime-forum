0126b2f69a8a2fa46c3d6392de4e01e2adcc43e9
adioz <adiozdaniel@gmail.com>
2025-02-07 17:51:39 +0300

    chores(routes): remove protection on home route - To allow all users to access home page

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -25,7 +25,6 @@ func (r *Routes) RegisterRoutes(mux *http.ServeMux) http.Handler {
 	// Auth routes
 	auth := middlewares.NewAuthContext(r.app)
 	mux.Handle("/api/posts", auth.AuthMiddleware(http.HandlerFunc(r.repo.PostsHandler)))
-	mux.Handle("/", auth.AuthMiddleware(http.HandlerFunc(r.repo.HomePageHandler)))
 	mux.Handle("/api/auth/check", auth.AuthMiddleware(http.HandlerFunc(r.repo.CheckAuth)))
 
 	// Page routes
@@ -37,6 +36,7 @@ func (r *Routes) RegisterRoutes(mux *http.ServeMux) http.Handler {
 	mux.HandleFunc("/api/auth/login", r.repo.LoginHandler)
 	mux.HandleFunc("/auth", r.repo.LoginPage)
 	mux.HandleFunc("/auth-sign-up", r.repo.SignUpPage)
+	mux.HandleFunc("/", r.repo.HomePageHandler)
 
 	// CORS middleware
 	handler := middlewares.CorsMiddleware(mux)
