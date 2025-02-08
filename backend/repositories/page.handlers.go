f2e8a498f9975d18cfc5400424cd6ebd659ab1a2
josie-opondo <josephineopondo5@gmail.com>
2025-02-08 17:36:30 +0300

    feat(profile): Implement profile page handler
    - Created a new profile page handler function to serve the corresponding profile page.
    - Integrated the handler into the application’s routing system.
    - Ensured proper error handling and response formatting.
    - Prepared the handler for future enhancements, such as dynamic user data loading.

--- backend/repositories/page.handlers.go
+++ backend/repositories/page.handlers.go
@@ -71,3 +71,25 @@ func (h *Repo) SignUpPage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Oops, something went wrong while rendering the page!", http.StatusInternalServerError)
 	}
 }
+
+// profile page
+func (h *Repo) ProfilePage(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodGet {
+    http.Error(w, "Oops, didn't understand what you are looking for", http.StatusForbidden)
+    }
+
+    tmpl, err := h.app.Tmpls.GetPage("profile.page.html")
+    if err != nil {
+		http.Error(w, "Oops, something went wrong!", http.StatusInternalServerError)
+		return
+    }
+
+    data := map[string]interface{} {
+        "Page": "profile",
+    }
+
+    err = tmpl.Execute(w, data)
+    if err != nil {
+		http.Error(w, "Oops, something went wrong while rendering the page!", http.StatusInternalServerError)
+    }
+}
