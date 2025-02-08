92f9b29aa764194ebb6da76fc27b9b610f2a15f4
adioz <adiozdaniel@gmail.com>
2025-02-08 23:55:08 +0300

    feat(response): create a page renderer - To seperate concerns. This relieves the page handlers of cluttering code.

---
+++ backend/response/models.go
@@ -0,0 +1,13 @@
+package response
+
+import "forum/forumapp"
+
+// Response represents a response implementations
+type Response struct {
+	app *forumapp.ForumApp
+}
+
+// NewResponse creates a new instance of Response
+func NewResponse(app *forumapp.ForumApp) *Response {
+	return &Response{app}
+}
---
+++ backend/response/renders.go
@@ -0,0 +1,14 @@
+package response
+
+import (
+	"net/http"
+)
+
+// RenderTemplate renders an HTML template
+func (m *Response) RenderTemplate(w http.ResponseWriter, templateName string, data interface{}) error {
+	tmpl, err := m.app.Tmpls.GetPage(templateName)
+	if err != nil {
+		return err
+	}
+	return tmpl.Execute(w, data)
+}
