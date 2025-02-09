d9bc9e8d13d28127d397e38619d65f0b26d870d1
Nungo <nued65@gmail.com>
2025-02-09 05:50:04 +0300

    test:add missing field test on the LoginHandler

--- backend/handlers/auth.handlers_test.go
+++ backend/handlers/auth.handlers_test.go
@@ -91,9 +91,26 @@ func TestLoginHandler(t *testing.T) {
 		fapp := &forumapp.ForumApp{}
 		fapp.Tmpls = tmplcach
 		jsonres := &response.JSONRes{}
-		incomplete := []byte(`{"username": "John Doe"}`)
+		incomplete := []byte(`{"username": "John@Doe.com"}`)
 		h := &Repo{app: fapp, res: jsonres}
 		req := httptest.NewRequest(http.MethodPost, "/api/auth/login", bytes.NewBuffer(incomplete))
+		w := httptest.NewRecorder()
+		h.LoginHandler(w, req)
+		if w.Code != http.StatusBadRequest {
+			t.Errorf("expected %d got %d", http.StatusBadRequest, w.Code)
+		}
+	})
+	t.Run("data invalid", func(t *testing.T) {
+		r := make(map[string]*template.Template)
+		r["home.page.html"] = template.New("home.page.html")
+		tmplcach := &forumapp.TemplateCache{Pages: r}
+
+		fapp := &forumapp.ForumApp{}
+		fapp.Tmpls = tmplcach
+		jsonres := &response.JSONRes{}
+		invalid := []byte(`{"email":"mamamboga@gmail.com","password":""}`) // missing password val
+		h := &Repo{app: fapp, res: jsonres}
+		req := httptest.NewRequest(http.MethodPost, "/api/auth/login", bytes.NewBuffer(invalid))
 		w := httptest.NewRecorder()
 		h.LoginHandler(w, req)
 		if w.Code != http.StatusBadRequest {
