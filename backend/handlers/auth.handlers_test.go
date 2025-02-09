7aa7fe088c0a30016df68136e6be5da132997069
Nungo <nued65@gmail.com>
2025-02-09 05:35:51 +0300

    test:add case of empty field test validity

--- backend/handlers/auth.handlers_test.go
+++ backend/handlers/auth.handlers_test.go
@@ -30,7 +30,7 @@ func TestRegisterHandler(t *testing.T) {
 			t.Errorf("expected %d got %d", http.StatusMethodNotAllowed, w.Code)
 		}
 	})
-	t.Run("data", func(t *testing.T) {
+	t.Run("data incomplete", func(t *testing.T) {
 		r := make(map[string]*template.Template)
 		r["home.page.html"] = template.New("home.page.html")
 		tmplcach := &forumapp.TemplateCache{Pages: r}
@@ -41,6 +41,24 @@ func TestRegisterHandler(t *testing.T) {
 		incomplete := []byte(`{"username": "John Doe"}`)
 		h := &Repo{app: fapp, res: jsonres}
 		req := httptest.NewRequest(http.MethodPost, "/api/auth/register", bytes.NewBuffer(incomplete))
+		w := httptest.NewRecorder()
+		h.RegisterHandler(w, req)
+		if w.Code != http.StatusBadRequest {
+			t.Errorf("expected %d got %d", http.StatusBadRequest, w.Code)
+		}
+	})
+	t.Run("data invalid", func(t *testing.T) {
+		r := make(map[string]*template.Template)
+		r["home.page.html"] = template.New("home.page.html")
+		tmplcach := &forumapp.TemplateCache{Pages: r}
+
+		fapp := &forumapp.ForumApp{}
+		fapp.Tmpls = tmplcach
+		jsonres := &response.JSONRes{}
+		invalid := []byte(`{"username": "John Doe","email":"mamamboga@gmail.com","password":""}`) // missing password val
+		h := &Repo{app: fapp, res: jsonres}
+		req := httptest.NewRequest(http.MethodPost, "/api/auth/register", bytes.NewBuffer(invalid))
+		req.Header.Set("Content-Type", "application/json")
 		w := httptest.NewRecorder()
 		h.RegisterHandler(w, req)
 		if w.Code != http.StatusBadRequest {
