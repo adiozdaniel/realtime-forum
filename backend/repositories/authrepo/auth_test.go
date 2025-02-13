9e79560575cb3837e930803d66e2febfcdac2496
nungo <nued65@gmail.com>
2025-02-13 13:36:15 +0300

    test:add RegisterLogin test for incomplete or missing data

--- backend/repositories/authrepo/auth_test.go
+++ backend/repositories/authrepo/auth_test.go
@@ -1,6 +1,8 @@
 package authrepo
 
 import (
+	"bytes"
+	"html/template"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,13 +12,28 @@ import (
 )
 
 func TestRegisterHandler(t *testing.T) {
-	authrepo := &AuthRepo{app: &forumapp.ForumApp{}, res: &shared.JSONRes{}, user: &UserService{}, shared: &shared.SharedConfig{}, Sessions: &Sessions{}}
 	t.Run("method", func(t *testing.T) {
+		authrepo := &AuthRepo{app: &forumapp.ForumApp{}, res: &shared.JSONRes{}, user: &UserService{}, shared: &shared.SharedConfig{}, Sessions: &Sessions{}}
 		req := httptest.NewRequest(http.MethodPost, "/api/auth/register", nil)
 		w := httptest.NewRecorder()
 		authrepo.RegisterHandler(w, req)
 		if req.Method != http.MethodPost {
 			t.Errorf("expected %d got %d", http.StatusMethodNotAllowed, w.Code)
+		}
+	})
+	t.Run("data incomplete or missing", func(t *testing.T) {
+		r := make(map[string]*template.Template)
+		r["home.page.html"] = template.New("home.page.html")
+		tmplcach := &forumapp.TemplateCache{Pages: r}
+		incomplete := []byte(`{"username": "John Doe"}`)
+
+		authrepo := &AuthRepo{app: &forumapp.ForumApp{Tmpls: tmplcach}, res: &shared.JSONRes{}, user: &UserService{}, shared: &shared.SharedConfig{}, Sessions: &Sessions{}}
+
+		req := httptest.NewRequest(http.MethodPost, "/api/auth/register", bytes.NewBuffer(incomplete))
+		w := httptest.NewRecorder()
+		authrepo.RegisterHandler(w, req)
+		if w.Code != http.StatusConflict {
+			t.Errorf("expected %d got %d", http.StatusBadRequest, w.Code)
 		}
 	})
 }
