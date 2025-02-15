6f3157fbbec528f0cf8378fbd0f04a037b74980b
adioz <adiozdaniel@gmail.com>
2025-02-15 17:21:26 +0300

    feat(auth): add localstorage support
    - Store authenticated user to the localstorage for global access and data persistance.

--- backend/static/scripts/authmiddleware.js
+++ backend/static/scripts/authmiddleware.js
@@ -1,23 +1,25 @@
-import { AuthService } from "./authservice.js"
+import { AuthService } from "./authservice.js";
 
 class Authmiddleware {
-    constructor(){
-        this.authService = new AuthService();
-        this.authButton = document.querySelector(".sign-in-button");
-    }
+	constructor() {
+		this.authService = new AuthService();
+		this.authButton = document.querySelector(".sign-in-button");
+	}
 }
 
 //Authchecker method to check if user is authenticated
-Authmiddleware.prototype.authChecker = async function(){
-    const isAuthenticated = await this.authService.isAuthenticated();
-    
-    if (isAuthenticated.error) console.log(isAuthenticated.message);
+Authmiddleware.prototype.authChecker = async function () {
+	const isAuthenticated = await this.authService.isAuthenticated();
 
-    if (isAuthenticated?.data) {
-        localStorage.setItem('userdata', isAuthenticated.data);
-        this.authButton.textContent = isAuthenticated.data ? "Sign Out" : "Sign In";
-    };
-}
+	if (isAuthenticated.error) console.log(isAuthenticated.message);
+
+	localStorage.removeItem("userdata");
+
+	if (isAuthenticated?.data) {
+		localStorage.setItem("userdata", JSON.stringify(isAuthenticated.data));
+		this.authButton.textContent = isAuthenticated.data ? "Sign Out" : "Sign In";
+	}
+};
 
 const authmiddleware = new Authmiddleware();
 
