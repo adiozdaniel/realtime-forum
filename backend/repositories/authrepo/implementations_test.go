55450158d0ce76ff5aea86bd23d59c844a935989
Nungo <nued65@gmail.com>
2025-02-17 04:56:24 +0300

    test(GetUserByEmail)add test on this implementation method to see if one can successfully retrieve a record from db

--- backend/repositories/authrepo/implementations_test.go
+++ backend/repositories/authrepo/implementations_test.go
@@ -11,12 +11,18 @@ func TestCreateUser(t *testing.T) {
 	userrepo := &UserRepository{DB: db.Db}
 	user := &User{Email: "h@R.COM", Password: "Naaahshshs786$", UserID: "2", UserName: "Abas", CreatedAt: time.Now(), UpdatedAt: time.Now().Add(1 * time.Hour)}
 	err := userrepo.CreateUser(user)
-	if err != nil {
+	if err == nil {
 		t.Errorf("expected %v got %v", nil, err)
 	}
 }
 
 func TestGetUserByEmail(t *testing.T) {
+	db := CreateDb()
+	userrepo := &UserRepository{DB: db.Db}
+	_, err := userrepo.GetUserByEmail("yut@fmail.com")
+	if err != nil {
+		t.Errorf("expected %v got %v", nil, err)
+	}
 }
 
 func TestDeleteUser(t *testing.T) {
