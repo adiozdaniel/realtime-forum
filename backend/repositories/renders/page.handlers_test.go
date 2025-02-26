7a79d4f77789844b10ff7389fefa26cb419286e9
nungo <nued65@gmail.com>
2025-02-26 13:51:58 +0300

    test(page.handlers):add httpmethod test for SignUpPageHandler

--- backend/repositories/renders/page.handlers_test.go
+++ backend/repositories/renders/page.handlers_test.go
@@ -83,3 +83,24 @@ func TestLoginPageHandler(t *testing.T) {
 		}
 	})
 }
+
+func TestSignUpPageHandler(t *testing.T) {
+	t.Run("Test httpmethod", func(t *testing.T) {
+		// Template cache
+		r := make(map[string]*template.Template)
+		r["home.page.html"] = template.New("home.page.html")
+		tmplcach := &forumapp.TemplateCache{Pages: r}
+
+		fapp := &forumapp.ForumApp{}
+		fapp.Tmpls = tmplcach
+		h := &RendersRepo{app: fapp}
+
+		req := httptest.NewRequest(http.MethodPost, "/auth-sign-up", nil)
+		writer := httptest.NewRecorder()
+
+		h.SignUpPageHandler(writer, req)
+		if writer.Code != http.StatusForbidden {
+			t.Errorf("Expected Method %d,got %d", http.StatusForbidden, writer.Code)
+		}
+	})
+}
