ce07c5b604249e5867703eecf4b46609f3961495
nungo <nued65@gmail.com>
2025-02-26 14:56:39 +0300

    test(page.handlers_test):add httpmethod test for AdminPageHandler

--- backend/repositories/renders/page.handlers_test.go
+++ backend/repositories/renders/page.handlers_test.go
@@ -151,3 +151,23 @@ func TestModeratorPageHandler(t *testing.T) {
 		}
 	})
 }
+
+func TestAdminPageHandler(t *testing.T) {
+	t.Run("Test httpmethod", func(t *testing.T) {
+		r := make(map[string]*template.Template)
+		r["home.page.html"] = template.New("home.page.html")
+		tmplcach := &forumapp.TemplateCache{Pages: r}
+
+		fapp := &forumapp.ForumApp{}
+		fapp.Tmpls = tmplcach
+		h := &RendersRepo{app: fapp}
+
+		req := httptest.NewRequest(http.MethodPost, "/admin", nil)
+		writer := httptest.NewRecorder()
+
+		h.AdminPageHandler(writer, req)
+		if writer.Code != http.StatusForbidden {
+			t.Errorf("Expected Method %d,got %d", http.StatusForbidden, writer.Code)
+		}
+	})
+}
